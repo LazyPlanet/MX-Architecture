c546cc629533b51862b2fd7bfb4aeaba8f1081e4
LazyPlanet <1211618464@qq.com>
2017-06-02 23:09:53 +0800

    代码优化

--- GameServer/Asset.cpp
+++ GameServer/Asset.cpp
@@ -45,7 +45,7 @@ bool AssetManager::Load()
 		}
 		else
 		{
-		       WARN("Load asset error, reduplicate message name:{}", msg->GetTypeName());
+			WARN("Load asset error, reduplicate message name:{}", msg->GetTypeName());
 		}
 	}
 	//加载所有资源数据
@@ -60,89 +60,108 @@ bool AssetManager::Load()
 
 bool AssetManager::LoadAssets(fs::path& full_path)
 {
-    	if (fs::exists(full_path))
-    	{
-        	fs::directory_iterator item_begin(full_path);
-        	fs::directory_iterator item_end;
+	if (fs::exists(full_path))
+	{
+		fs::directory_iterator item_begin(full_path);
+		fs::directory_iterator item_end;
 
-        	for ( ; item_begin != item_end; item_begin++)
-        	{
-            	if (fs::is_directory(*item_begin))
-            	{
-					std::string sub_dir_str(item_begin->path().string());
-					fs::path sub_dir(sub_dir_str);
-					LoadAssets(sub_dir);
-            	}
-            	else
-            	{
-					const std::string& filename = item_begin->path().string();
-					//////打开文件
-					std::fstream file(filename.c_str(), std::ios::in | std::ios::binary);
-					if (!file) return false; 	//如果一个有问题就退出
+		for ( ; item_begin != item_end; item_begin++)
+		{
+			if (fs::is_directory(*item_begin))
+			{
+				std::string sub_dir_str(item_begin->path().string());
+				fs::path sub_dir(sub_dir_str);
+				LoadAssets(sub_dir);
+			}
+			else
+			{
+				const std::string& filename = item_begin->path().string();
+				//////打开文件
+				std::fstream file(filename.c_str(), std::ios::in | std::ios::binary);
+				if (!file) continue; 	//如果一个有问题就退出
 
-					int32_t size = 0;
+				int32_t size = 0;
 
-					file >> size;
-					if (size == 0 || size > 1024) return false;	//理论上单个文件不会超过1024字节
+				file >> size;
+				if (size == 0 || size > 1024) continue;	//理论上单个文件不会超过1024字节
 
-					char content[1024];
-					file.readsome(content, size);
+				char content[1024];
+				file.readsome(content, size);
 
-					std::string directory_string = item_begin->path().parent_path().string();
-					if (directory_string == "") return false;
+				std::string directory_string = item_begin->path().parent_path().string();
+				if (directory_string.empty()) continue;
 
-					int32_t found_pos = directory_string.find_last_of("/");
-					const std::string& message_name = directory_string.substr(found_pos + 1);	//MESSAGE名称即为文件夹名称
+				int32_t found_pos = directory_string.find_last_of("/");
+				const std::string& message_name = directory_string.substr(found_pos + 1);	//MESSAGE名称即为文件夹名称
 
-					const pb::Descriptor* descriptor = this->_file_descriptor->FindMessageTypeByName(message_name);
-					if (!descriptor) return false;
+				const pb::Descriptor* descriptor = this->_file_descriptor->FindMessageTypeByName(message_name);
+				if (!descriptor) continue;
 
-					const pb::Message* msg = pb::MessageFactory::generated_factory()->GetPrototype(descriptor);
-					if (!msg) return false;
+				const pb::Message* msg = pb::MessageFactory::generated_factory()->GetPrototype(descriptor);
+				if (!msg) continue;
 
-					pb::Message* message = msg->New();
-					message->ParseFromArray(content, size);
+				pb::Message* message = msg->New();
+				message->ParseFromArray(content, size);
 
-					//////关闭文件
-					file.close();
+				//////关闭文件
+				file.close();
 
-					////////////////////////////////////////////
-					const pb::FieldDescriptor* type_field = message->GetDescriptor()->FindFieldByName("type_t");
-					if (!type_field) return false; //如果一个有问题就退出
+				////////////////////////////////////////////
+				const pb::FieldDescriptor* type_field = message->GetDescriptor()->FindFieldByName("type_t");
+				if (!type_field) continue; //如果一个有问题就退出
 
-					int64_t global_id = 0;
+				int64_t global_id = 0;
 
-					const pb::FieldDescriptor* prop_field = message->GetDescriptor()->FindFieldByName("common_prop");
-					if (prop_field) //普通资源
+				const pb::FieldDescriptor* prop_field = message->GetDescriptor()->FindFieldByName("common_prop");
+				if (prop_field) //普通资源
+				{
+					const pb::Message& prop_message = message->GetReflection()->GetMessage(*message, prop_field);
+					const pb::FieldDescriptor* global_id_field = prop_message.GetDescriptor()->FindFieldByName("global_id");
+					if (!global_id_field) 
 					{
-						const pb::Message& prop_message = message->GetReflection()->GetMessage(*message, prop_field);
-						const pb::FieldDescriptor* global_id_field = prop_message.GetDescriptor()->FindFieldByName("global_id");
-						if (!global_id_field) return false;
-						global_id = prop_message.GetReflection()->GetInt64(prop_message, global_id_field);
+						ERROR("加载普通资源失败, 原因：未能找到变量[global_id]");
+						return false;
 					}
-					else //物品资源
+					global_id = prop_message.GetReflection()->GetInt64(prop_message, global_id_field);
+				}
+				else //物品资源
+				{
+					const pb::FieldDescriptor* item_prop_field = message->GetDescriptor()->FindFieldByName("item_common_prop");
+					if (!item_prop_field) 
 					{
-						const pb::FieldDescriptor* item_prop_field = message->GetDescriptor()->FindFieldByName("item_common_prop");
-						if (!item_prop_field) return false;
-
-						const pb::Message& item_prop_message = message->GetReflection()->GetMessage(*message, item_prop_field);
-						prop_field = item_prop_message.GetDescriptor()->FindFieldByName("common_prop");
-						if (!prop_field) return false;
-
-						const pb::Message& prop_message = item_prop_message.GetReflection()->GetMessage(item_prop_message, prop_field);
-						const pb::FieldDescriptor* global_id_field = prop_message.GetDescriptor()->FindFieldByName("global_id");
-						if (!global_id_field) return false;
-						global_id = prop_message.GetReflection()->GetInt64(prop_message, global_id_field);
+						ERROR("加载物品资源失败, 原因：未能找到变量[item_common_prop]");
+						return false;
 					}
-					////////////////////////////////////////////加载到全局唯一表
-					_assets.emplace(global_id, message);
 
-					////////////////////////////////////////////加载到类型表
-					int32_t type_t = type_field->default_value_enum()->number();
-					_assets_bytypes[type_t].emplace(message);
-            	}
-        	}
-    	}
+					const pb::Message& item_prop_message = message->GetReflection()->GetMessage(*message, item_prop_field);
+					prop_field = item_prop_message.GetDescriptor()->FindFieldByName("common_prop");
+					if (!prop_field) 
+					{
+						ERROR("加载物品资源失败, 原因：未能找到变量[common_prop]");
+						return false;
+					}
+
+					const pb::Message& prop_message = item_prop_message.GetReflection()->GetMessage(item_prop_message, prop_field);
+					const pb::FieldDescriptor* global_id_field = prop_message.GetDescriptor()->FindFieldByName("global_id");
+					if (!global_id_field) 
+					{
+						ERROR("加载物品资源失败, 原因：未能找到变量[global_id]");
+						return false;
+					}
+					global_id = prop_message.GetReflection()->GetInt64(prop_message, global_id_field);
+				}
+				////////////////////////////////////////////加载到全局唯一表
+
+				DEBUG("加载资源成功，资源类型:{} 全局ID:{}", type_field->name(), global_id);
+
+				_assets.emplace(global_id, message);
+
+				////////////////////////////////////////////加载到类型表
+				int32_t type_t = type_field->default_value_enum()->number();
+				_assets_bytypes[type_t].emplace(message);
+			}
+		}
+	}
 
 	return true;
 } 
