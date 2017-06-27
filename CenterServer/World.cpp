70a416738959599e98d612faf98973ed213ed77e
LazyPlanet <1211618464@qq.com>
2017-06-27 08:17:34 +0800

    中心服

---
+++ CenterServer/World.cpp
@@ -0,0 +1,64 @@
+#include "World.h"
+#include "Protocol.h"
+#include "PlayerName.h"
+#include "MXLog.h"
+#include "Activity.h"
+
+namespace Adoter
+{
+
+const Asset::CommonConst* g_const = nullptr;
+
+bool World::Load()
+{
+	//协议初始化：必须最先初始化
+	if (!ProtocolInstance.Load()) 
+	{
+		ERROR("ProtocolInstance load error.");
+		return false;
+	}
+	//数据初始化：必须最先初始化
+	if (!AssetInstance.Load()) 
+	{
+		ERROR("AssetInstance load error.");
+		return false;
+	}
+
+	//
+	//不依赖顺序的数据初始化
+	//
+	
+	if (!NameInstance.Load())
+	{
+		ERROR("NameInstance load error.");
+		return false;
+	}
+
+	//
+	//游戏内初始化
+	//
+
+	//特殊ID定义表
+	pb::Message* message = AssetInstance.Get(458753); 
+	g_const = dynamic_cast<const Asset::CommonConst*>(message); 
+	if (!g_const) return false; //如果没有起不来
+
+	//活动
+	if (!ActivityInstance.Load())
+	{
+		ERROR("ActivityInstance load error.");
+		return false;
+	}
+	
+	return true;
+}
+
+//世界中所有刷新都在此(比如刷怪，拍卖行更新...)，当前周期为50MS.
+void World::Update(int32_t diff)
+{
+	++_heart_count;
+
+	ActivityInstance.Update(diff);
+}
+	
+}
