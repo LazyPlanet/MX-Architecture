a83fceb7192938b062937567d50ccd95574538cf
LazyPlanet <1211618464@qq.com>
2018-02-20 11:09:43 +0800

    牌数据加载类型

--- GameServer/Game.cpp
+++ GameServer/Game.cpp
@@ -2231,6 +2231,9 @@ bool GameManager::Load()
 		Asset::MJCard* asset_card = dynamic_cast<Asset::MJCard*>(message); 
 		if (!asset_card) return false;
 		
+		if (asset_card->card_type() != Asset::CARD_TYPE_WANZI && asset_card->card_type() != Asset::CARD_TYPE_BINGZI && asset_card->card_type() != Asset::CARD_TYPE_TIAOZI 
+				&& asset_card->card_type() == Asset::CARD_TYPE_FENG && asset_card->card_type() != Asset::CARD_TYPE_JIAN) continue;
+		
 		for (int k = 0; k < asset_card->group_count(); ++k) //4组，麻将每张牌有4张
 		{
 			int32_t cards_count = std::min(asset_card->cards_count(), asset_card->cards_size());
